1fadc68955e4c5fc7d01b1dd97e6217d62a22efe
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-12 03:34:07 +0100

    connected cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+      api_key: env('CLOUDINARY_API_KEY'),
+      api_secret: env('CLOUDINARY_API_SECRET'),
+    },
+  },
+});
+
