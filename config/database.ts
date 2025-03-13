089cbde67d8f5e4ec9797f02515fd15c15a03d74
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-13 12:27:22 +0100

    config

--- config/database.ts
+++ config/database.ts
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'), // Default to 'postgres' if not set
+    client: 'postgres', // Default to 'postgres' if not set
     connection: {
       // If you're using DATABASE_URL, uncomment the next line:
       uri: env('DATABASE_URL'),
 
       // If you want to specify individual environment variables:
-      // host: env('DATABASE_HOST'),          // Use the PGHOST value (e.g., switchback.proxy.rlwy.net)
-      port: env.int('DATABASE_PORT', 5432), // Default port for PostgreSQL is 5432
-      // user: env('DATABASE_USERNAME'),      // e.g., postgres
-      // password: env('DATABASE_PASSWORD'),  // Your password
-      // database: env('DATABASE_NAME'),      // e.g., railway
-      // ssl: env.bool('DATABASE_SSL', true),  // You can set to true if SSL is required
+      host: env('PGHOST', '127.0.01'),          // Use the PGHOST value (e.g., switchback.proxy.rlwy.net)
+      port: env.int('PGPORT', 5432), // Default port for PostgreSQL is 5432
+      user: env('PGUSER'),      // e.g., postgres
+      password: env('PGPASSWORD'),  // Your password
+      database: env('PGDATABASE', 'strapi'),      // e.g., railway
+      ssl: env.bool('DATABASE_SSL', true),  // You can set to true if SSL is required
     },
   },
 });
