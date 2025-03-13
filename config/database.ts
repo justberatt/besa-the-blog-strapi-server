2b00beab5554485491d4370c1769112e6639f9ac
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-13 11:36:49 +0100

    config

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: env('DATABASE_CLIENT'),
     connection: {
       uri: env('DATABASE_URL'),  // Using DATABASE_URL directly for simplicity
-      host: env('DATABASE_HOST'),
+      host: env('PGHOST'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
